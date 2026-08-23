683919a01e08ea3c69135916aae9bc0e9d65ba39
Hptrivia <rasheed-ayodeji.owolabi@iubh.de>
2026-08-23 09:35:32 +0000

    ggg

--- assets/admob.js
+++ assets/admob.js
@@ -14,7 +14,7 @@
 //   2. change ios: 'off' → 'live' here.
 const ADMOB_MODE_BY_PLATFORM = {
   ios: 'live',
-  android: 'live',
+  android: 'test',
 };
 const _ADMOB_PLATFORM = window.Capacitor?.getPlatform?.();
 
